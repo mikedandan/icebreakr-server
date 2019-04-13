59f550c07bbecfa91c617e86fffbe624d9630a1c
Sam Wang <samuelwangkc@gmail.com>
2019-04-13 15:56:45 -0700

    added create user route

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    actualName:{
+    password:{
         type: String,
         required: true
     },
