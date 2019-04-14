17f4287d449386e7d49229abe3da306921351a90
mikedandan <mikedandan2@gmail.com>
2019-04-14 16:12:17 -0700

    returned to heroku server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const axios = require("axios");
 // Require all models
 const db = require("./models");
 
-var PORT = process.env.PORT || 10255;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var routes = require("./controller/apiRoutes.js");
@@ -33,19 +33,19 @@ app.use(routes);
 // app.use(cors());
 
 // //// Connect to the Mongo DB via Heroku ////
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-// mongoose.connect(MONGODB_URI);
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // Connect to the Mongo DB via Azure ////
 
-mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb",  {
-    auth: {
-      user: process.env.COSMODDB_USER,
-      password: process.env.COSMOSDB_PASSWORD
-    }
-  })
-  .then(() => console.log('Connection to CosmosDB successful'))
-  .catch((err) => console.error(err));
+// mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb",  {
+//     auth: {
+//       user: process.env.COSMODDB_USER,
+//       password: process.env.COSMOSDB_PASSWORD
+//     }
+//   })
+//   .then(() => console.log('Connection to CosmosDB successful'))
+//   .catch((err) => console.error(err));
 
 // HTML Routes
 
@@ -61,17 +61,9 @@ http.listen(PORT, function () {
     console.log(`listening on *: ${PORT}`);
 });
 
-// app.get('/api/users', function (req, res) {
-//     db.User.find({}).then((user) => {
-//         res.json(user);
-//     }).catch(err => console.log(err));
-// });
-app.get('/users', function (req, res) {
-    User.find({}), function(err, foundUser) {
-        foundUser.forEach(us => console.log("Found User: " + JSON.stringify(us)));
-    }
-})
+app.get('/api/users', function (req, res) {
+    db.User.findAll({}).then((user) => {
+        res.json(user);
+    }).catch(err => console.log(err));
+});
 
-// User.find({ 'children.gender' : "male"}, function(err, foundFamily){
-//     foundFamily.forEach(fam => console.log("Found Family: " + JSON.stringify(fam)));
-// });
