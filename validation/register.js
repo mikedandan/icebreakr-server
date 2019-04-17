f257f95de8ac654aa86ab7ce5ed9a65c7519db67
Mike <mikedandan2@gmail.com>
2019-04-16 20:04:00 -0800

    updated register

--- validation/register.js
+++ validation/register.js
@@ -3,7 +3,7 @@ const isEmpty = require("is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 // Convert empty fields to an empty string so we can use validator functions
-  data.displayName = !isEmpty(data.displayName) ? data.displayName : "";
+  data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   // data.password2 = !isEmpty(data.password2) ? data.password2 : "";
