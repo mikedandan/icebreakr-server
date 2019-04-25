f6f5ed9b166d9dab3e5ee370d136326dadc0f9c7
Derek Moore <moore8577@gmail.com>
2019-04-24 19:56:24 -0700

    testing the backend

--- public/joinNs.js
+++ public/joinNs.js
@@ -35,13 +35,8 @@ function joinNs(endpoint) {
         joinRoom(groupRoomName);
 
     })
-<<<<<<< HEAD
-    nsSocket.on('messageToClients', (msg) => {
-        console.log(msg)
-=======
     nsSocket.on('messageToClients',(msg)=>{
         // console.log(msg)
->>>>>>> 64765606d0ebcd00e4f86b3ffc77ef91909c253c
         const newMsg = buildHTML(msg);
         document.querySelector('#messages').innerHTML += newMsg;
     })
@@ -51,43 +46,10 @@ function joinNs(endpoint) {
 function formSubmission(event) {
     event.preventDefault();
     const newMessage = document.querySelector('#user-message').value;
-<<<<<<< HEAD
-
-    nsSocket.emit('newMessageToServer', { text: newMessage })
-    $.post("http://localhost:3000/api/message/new", {
-        "nickName": "BOB SANDERS",
-        "message": newMessage,
-        "picture": "test.png",
-        "userID": "12345",
-        "lon": 55555,
-        "lat": 11111,
-        "namespace": "group",
-        "date": Date.now()
-    }, function () {
-        console.log("sent to db")
-    });
-    // axios.post('http://localhost:3000/api/message/new', {
-    //     "nickName": "Sam",
-    //     "message": newMessage,
-    //     "picture": "test.png",
-    //     "userID": "12345",
-    //     "lon": 55555,
-    //     "lat": 11111,
-    //     "namespace": "group",
-    //     "date": Date.now()
-    // })
-    //     .then(async function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
-=======
     nsSocket.emit('newMessageToServer',{text: newMessage})
     console.log(`This is sending a message to the SERVER using 'newMessagetoServer' on joinNs.js
     ---------------------
     the message is: ${newMessage}`)
->>>>>>> 64765606d0ebcd00e4f86b3ffc77ef91909c253c
 }
 
 function buildHTML(msg) {
