f243e2237d06d7334ca17defd9622c262d5a075d
Mike <mikedandan2@gmail.com>
2019-06-04 20:02:26 -0800

    reverting server.js to heroku

--- server.js
+++ server.js
@@ -11,46 +11,6 @@ const events = require("./routes/event");
 let namespaces = require('./data/namespaces');
 const path = require("path");
 
-var crypto = require("crypto");
-var moment = require("moment");
-
-let masterKey = "RTBA2c8HivHZ0un7wnl23rC1QNyzOUIpOiKGIVUJkuQbw3Vd8xVxIdhbI8IAEmfhjT5djis25JL6ODNZb0xLkw==";
-let dateInRfc7231Format = moment().format("ddd, DD MMM YYYY HH:mm:ss");
-let dateWithTimeZone = dateInRfc7231Format + " GMT";
-console.debug(dateWithTimeZone);
-// list all databases
-//let result = getAuthorizationTokenUsingMasterKey("GET", "", "dbs", dateWithTimeZone, masterKey);
-
-// list all collections within a db
-//let result = getAuthorizationTokenUsingMasterKey("GET", "dbs/demodb", "colls", dateWithTimeZone, masterKey);
-
-// create new collection
-//let result = getAuthorizationTokenUsingMasterKey("POST", "dbs/demodb", "colls", dateWithTimeZone, masterKey);
-
-// list all documents
-//let result = getAuthorizationTokenUsingMasterKey("GET", "dbs/demodb/colls/student", "docs", dateWithTimeZone, masterKey);
-
-let result = getAuthorizationTokenUsingMasterKey("POST", "dbs/demodb/colls/student", "docs", dateWithTimeZone, masterKey);
-
-
-console.debug(result);
-
-function getAuthorizationTokenUsingMasterKey(verb, resourceId, resourceType, date, masterKey) {  
-    var key = new Buffer(masterKey, "base64");  
-
-    var text = (verb || "").toLowerCase() + "\n" +   
-               (resourceType || "").toLowerCase() + "\n" +   
-               (resourceId || "") + "\n" +   
-               (date || "").toLowerCase() + "\n" +   
-               "" + "\n";
-
-    var body = new Buffer(text, "utf8");  
-    var signature = crypto.createHmac("sha256", key).update(body).digest("base64");
-    var MasterToken = "master";  
-    var TokenVersion = "1.0";  
-
-    return encodeURIComponent("type=" + MasterToken + "&ver=" + TokenVersion + "&sig=" + signature);  
-}
 
 const env = require('dotenv').config();
 // var cors = require('cors');
@@ -63,7 +23,7 @@ const axios = require("axios");
 // Require all models
 // const db = require("./models");
 
-const PORT = process.env.PORT || 10255;
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express
 // var routes = require("./controller/apiRoutes.js");
@@ -81,8 +41,8 @@ app.use(express.static("public"));
 // app.use(cors());
 
 // //// Connect to the Mongo DB via Heroku ////
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/icebreakr";
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/icebreakr";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 // Passport middleware
@@ -95,27 +55,15 @@ app.use("/api/message", messages);
 app.use("/api/event", events);
 
 // Connect to the Mongo DB via Azure ////
-mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb", {
-  auth: {
-    user: process.env.COSMODDB_USER,
-    password: process.env.COSMOSDB_PASSWORD
-  }
-})
-.then(() => console.log('Connection to CosmosDB successful'))
-.catch((err) => console.error(err));
-// mongoose.connect("mongodb://icebreakr:WOpa2isrtPdg7E6HzE4O16pr8BstnfDZT4UVSG29kdWMcF6LscO9t0G1O9uO4Ppq5Ta4DOoFgYKFRjQxlsVnhw%3D%3D@icebreakr.documents.azure.com:10255/?ssl=true"?ssl=true&replicaSet=globaldb",  {
+
+// mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb",  {
 //     auth: {
-//       user: icebreakr,
-//       password: WOpa2isrtPdg7E6HzE4O16pr8BstnfDZT4UVSG29kdWMcF6LscO9t0G1O9uO4Ppq5Ta4DOoFgYKFRjQxlsVnhw==
+//       user: process.env.COSMODDB_USER,
+//       password: process.env.COSMOSDB_PASSWORD
 //     }
 //   })
 //   .then(() => console.log('Connection to CosmosDB successful'))
 //   .catch((err) => console.error(err));
-
-// var mongoClient = require("mongodb").MongoClient;
-// mongoClient.connect("mongodb://icebreakr:WOpa2isrtPdg7E6HzE4O16pr8BstnfDZT4UVSG29kdWMcF6LscO9t0G1O9uO4Ppq5Ta4DOoFgYKFRjQxlsVnhw%3D%3D@icebreakr.documents.azure.com:10255/?ssl=true", function (err, client) {
-//   client.close();
-// });
 
 // HTML Routes
 
@@ -236,7 +184,7 @@ http.listen(PORT, function () {
 
 
 app.get('/', (req, res) => {
-  res.json("Hello welcome to the Icebreakr Servers!");
+  res.json("Hello welcome to the Icebreakr Server!");
 });
 
 app.get('/testChat', function (req, res) {
@@ -247,4 +195,3 @@ app.get("/", function(req, res) {
   // res.send("Welcome to the Star Wars Page!")
   res.sendFile(path.join(__dirname, "view.html"));
 });
-
